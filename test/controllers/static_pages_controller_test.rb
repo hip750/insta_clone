0aae94520ee89b5e51468d88b3b2a1e107c077c8
Hajime Kawajiri <hajime_kawajiri@outlook.jp>
2020-11-14 05:14:45 +0000

    ”âOnece finished layout

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,17 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get root" do
-    get root_url
-    assert_response :success
-  end
 
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
+    assert_select "title", "Insta Clone App"
+  end
+
+  test "should get contact" do
+    get contact_path
+    assert_response :success
+    assert_select "title", "Contact | Insta Clone App"
   end
 
 end
