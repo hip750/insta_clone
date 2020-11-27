606a30d6fa8b3d7a2dd48955255080136f9a27d1
Hajime Kawajiri <hajime_kawajiri@outlook.jp>
2020-11-27 13:13:44 +0000

    mailgun fixed

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -20,17 +20,6 @@ class PasswordResetsController < ApplicationController
   end
 
   def edit
-    user = User.find_by(email: params[:email])
-    if user && !user.activated? && user.authenticated?(:activation, params[:id])
-      user.update_attribute(:activated,    true)
-      user.update_attribute(:activated_at, Time.zone.now)
-      log_in user
-      flash[:success] = "登録が完了しました！"
-      redirect_to user 
-    else
-      flash[:danger] = "リンクが無効です。"
-      redirect_to root_url
-    end
   end
 
 def update
