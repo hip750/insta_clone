2b1ef0c3e9193bc1e9a01da4a83e1fc7fca380aa
Hajime Kawajiri <hajime_kawajiri@outlook.jp>
2020-12-12 01:38:24 +0000

    SEO friendly

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,6 +8,28 @@ module ApplicationHelper
     end
   end
 
+  # descriptions
+  def default_meta_tags
+    { site: 'Insta Clone App',
+      title: 'Insta Clone App',
+      reverse: true,
+      separator: '|',
+      description: 'Insta Clone Appは好きな写真を投稿してみんなでシェアできる無料SNSです。',
+      keywords: 'SNS, 写真, 画像, シェア',
+      canonical: request.original_url,
+      noindex: ! Rails.env.production?,
+      og: {
+        site_name: 'Insta Clone App',
+        title: 'Insta Clone App',
+        description: 'Insta Clone Appは好きな写真を投稿してみんなでシェアできる無料SNSです。', 
+        type: 'website',
+        url: request.original_url,
+        image: image_url('insta_top.jpg'),
+        locale: 'ja_JP',
+      }
+    }
+  end
+
   def notification_form(notification)
     @visitor = notification.visitor
     @comment = nil
